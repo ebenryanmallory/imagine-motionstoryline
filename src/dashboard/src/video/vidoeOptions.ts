3c435c83a7377ffdca3863c647870be44f415a70
Eben Mallory <ebenryanmallory@gmail.com>
2024-04-05 12:28:20 -0500

    added basic conditional logic to video view

--- src/dashboard/src/video/vidoeOptions.ts
+++ src/dashboard/src/video/vidoeOptions.ts
@@ -14,8 +14,18 @@ const setupEventListeners = () => {
   const pollButton = document.querySelector('#poll-video');
   (pollButton as HTMLElement).addEventListener('click', pollVideo);
   onChange('videos', (newValue: string[], oldValue: string[]) => {
-    const container = document.querySelector('#video-container');
-    (container as HTMLElement).innerHTML = reactiveVideo(newValue[state.currentSlide]);
+    const container = document.querySelector('#video-container') as HTMLElement;
+    container.innerHTML = reactiveVideo(newValue[state.currentSlide]);
+  });
+  onChange('generationID', (newValue: string[], oldValue: string[]) => {
+    const generateButton = document.querySelector('#generate-video') as HTMLElement;
+    const pollButton = document.querySelector('#poll-video') as HTMLElement;
+    const helerText = document.querySelector('#helper_text') as HTMLParagraphElement;
+    if (state.generationID[state.currentSlide]) {
+      generateButton.classList.add('hidden')
+      pollButton.classList.remove('hidden')
+      helerText.innerText = "Videos take a couple of minutes to generate. Check to see if it ready by clicking poll."
+    }
   });
 };
 
@@ -26,8 +36,10 @@ export const videoOptions = () => {
     <sl-spinner class="spinner hidden" style="font-size: 3rem; --indicator-color: lightgreen; --track-color: darkgreen;"></sl-spinner>
     <p>Generate video from current image.</p>
     <sl-button id="generate-video" class="py-2">Generate Video</sl-button>
-    <sl-button id="poll-video" class="py-2">Poll Video</sl-button>
-    <p>Videos take a couple of minutes to generate. Check to see if it ready by clicking poll.
+    <sl-button id="poll-video" class="py-2 hidden">Poll Video</sl-button>
+    <p id='helper_text'>Click to begin a background render for your selected image.</p>
   </sl-card>
-  <div id="video-container" class="w-full py-3 overflow-hidden mb-2"></div>
+  <div id="video-container" class="w-full py-3 overflow-hidden mb-2">
+    ${state.videos[state.currentSlide] && reactiveVideo(state.videos[state.currentSlide])}
+  </div>
 `};
